dbf4cdab98c090573966553a222e3d438af941f0
firas-debich <debichf02@gmail.com>
2022-01-17 22:20:34 +0100

    start working on functionality

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("hello world")
+switch(window.location.hostname){
+    case "www.youtube.com":
+        alert("you are in youtube");
+    break;
+    case "www.facebook.com":
+        alert("you are in facebook");
+    break; case "www.linkedin.com":
+    alert("you are in linkedin");
+break;
+
+}
