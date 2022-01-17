99dab3d99a1fd39949c84749f5f5c7a66732d94d
firas-debich <debichf02@gmail.com>
2022-01-17 22:29:46 +0100

    gnerate Html function done

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+const generateHtml = (pageName)=>{
+    return `
+    <div id="clouds">
+
+    <div class="cloud x1"></div>
+    <div class="cloud x1_5"></div>
+    <div class="cloud x2"></div>
+    <div class="cloud x3"></div>
+    <div class="cloud x4"></div>
+    <div class="cloud x5"></div>
+    <div class="c">
+        <div class="_404">404</div>
+        <hr/>
+        <div class="_1"> Get Back work</div>
+        <div class="_2"> studing > ${pageName}</div>
+    
+    </div>
+    
+    </div>
+
+    
+    `
+
+
+
+}
+
 switch(window.location.hostname){
     case "www.youtube.com":
-        document.body.innerHTML="<button> Click Here</button>"
+       
         alert("you are in youtube");
     break;
     case "www.facebook.com":
